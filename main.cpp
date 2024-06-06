65cc923775b73e240bbd47a572285d8c38784b46
purple-leopard <97757699+purple-leopard@users.noreply.github.com>
2024-06-06 16:40:00 +0000

    Implement A and HeapA

--- main.cpp
+++ main.cpp
@@ -13,14 +13,17 @@ New/This/Pointers/References conclusion
      on the lines below it, write a struct named 'HeapA' that correctly shows how to own an instance of 'A' 
          on the heap without leaking, without using smart pointers. 
  */
+struct A {};
 
-
-
-
-
-
-
-
+struct HeapA
+{
+    HeapA() : aPtr(new A) {}
+    ~HeapA()
+    {
+        delete aPtr;
+    }
+    A* aPtr = nullptr;
+}
 
 
 
